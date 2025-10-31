a40cd91f99b3aa31d480ac5dcbf3ee6521ba735f
louisquantran <louis.quantran@gmail.com>
2025-10-31 16:46:12 -0700

    Changed from 9 bits address to 32 bits address

--- I_Cache.sv
+++ I_Cache.sv
@@ -23,18 +23,18 @@
 module ICache (
     input logic clk,
     input logic reset,
-    input logic [8:0] address, 
+    input logic [31:0] address, 
     output logic [31:0] instruction
 );
     //shift address right by 2
-    logic [31:0] instr_mem[511:0];
+    logic [31:0] instr_mem[0:551];
     
     // initialize instruction memory
     // initial begin
     //      $readmemh("program.hex", mem);
     // end
     always_ff @(posedge clk) begin
-        if (reset) instruction <= '0;
+        if (reset) instruction <= 32'b0;
         else instruction <= instr_mem[address>>2];
     end
 endmodule
