fc2c8af37ea0049ec671fb770d9fc0842488d551
GaussianCat <zheng10plus@gmail.com>
2025-11-04 18:24:40 -0800

    Add FUs signals and fix handshake logic

--- decode.sv
+++ decode.sv
@@ -21,9 +21,14 @@ module decode(
     output logic [4:0] rd,
     output logic [31:0] imm,
     output logic [2:0] ALUOp,
-    output logic [6:0] opcode
+    output logic [6:0] opcode,
+    output logic fu_mem,
+    output logic fu_alu
     // Harzard detect signal?
     );
+    
+    // track state
+    logic full;
     
     // Buffered signals
     logic [31:0] pc_buf;
@@ -35,6 +40,9 @@ module decode(
     logic [31:0] imm_buf;
     logic [2:0] ALUOp_buf;
     logic [6:0] opcode_buf;
+    
+    logic fu_mem_buf;
+    logic fu_alu_buf;
 
     //  Future signals
     logic [4:0] rs1_next;
@@ -43,10 +51,12 @@ module decode(
     logic [31:0] imm_next;
     logic [2:0] ALUOp_next;
     logic [6:0] opcode_next;
+    logic fu_mem_next;
+    logic fu_alu_next;
     
     // Combinational Section
     assign pc_out = pc_buf;
-    assign ready_in = ready_out || !valid_out_buf;
+    assign ready_in = ready_out && !valid_out_buf;
     assign valid_out = valid_out_buf;
     assign rs1 = rs1_buf;
     assign rs2 = rs2_buf;
@@ -54,6 +64,8 @@ module decode(
     assign imm = imm_buf;
     assign ALUOp = ALUOp_buf;
     assign opcode = opcode_buf;
+    assign fu_mem = fu_mem_buf;
+    assign fu_alu = fu_alu_buf;
 
     ImmGen immgen_dut (
         .instr(instr),
@@ -66,36 +78,52 @@ module decode(
         .rs2(rs2_next),
         .rd(rd_next),
         .ALUOp(ALUOp_next),
-        .opcode(opcode_next)
+        .opcode(opcode_next),
+        .fu_mem(fu_mem_next),
+        .fu_alu(fu_alu_next)
     );
 
     always_comb begin
         if (reset) begin
-            pc_buf <= 32'b0;
-            valid_out_buf <= 1'b0;
+            pc_buf = 32'b0;
+            valid_out_buf = 1'b0;
             
-            rs1_buf <= 5'b0;
-            rs2_buf <= 5'b0;
-            rd_buf <= 5'b0;
-            imm_buf<= 32'b0;
-            ALUOp_buf <= 3'b0;
-            opcode_buf <= 7'b0;
+            rs1_buf = 5'b0;
+            rs2_buf = 5'b0;
+            rd_buf = 5'b0;
+            imm_buf = 32'b0;
+            ALUOp_buf = 3'b0;
+            opcode_buf = 7'b0;
+            
+            full = 1'b0;
         end else begin
             // Handle upstream
             if (valid_in && ready_in) begin
-                pc_buf <= pc_in;
-                valid_out_buf <= 1'b1;
+                pc_buf = pc_in;
+                valid_out_buf = 1'b1;
                 
                 // all signals handled by decoder and immGen
-                rs1_buf <= rs1_next;
-                rs2_buf <= rs2_next;
-                rd_buf <= rd_next;
-                opcode_buf <= opcode_next;
-                imm_buf <= imm_next;
-                ALUOp_buf <= ALUOp_next;
-            end else if (ready_out && valid_out) begin
+                rs1_buf = rs1_next;
+                rs2_buf = rs2_next;
+                rd_buf = rd_next;
+                opcode_buf = opcode_next;
+                imm_buf = imm_next;
+                ALUOp_buf = ALUOp_next;
+                fu_mem_buf = fu_mem_next;
+                fu_alu_buf = fu_alu_next;
+                
+                full = 1'b1;
+            end else begin
+                // do nothing (keep to avoid bug)
+            end
+            
             // Handle downstream
+            if (ready_out && valid_out && full) begin
+                full = 1'b0;
+            end else if (!ready_out && valid_out && !full) begin
                 valid_out_buf <= 1'b0;
+            end else begin
+                // do nothing (keep to avoid bug)
             end
         end
     end
--- signal_decode.sv
+++ signal_decode.sv
@@ -6,7 +6,9 @@ module signal_decode(
     output logic [4:0] rs2,
     output logic [4:0] rd,
     output logic [2:0] ALUOp,
-    output logic [6:0] opcode
+    output logic [6:0] opcode,
+    output logic fu_mem,
+    output logic fu_alu
     );
     
     logic [4:0] rs1_next;
@@ -15,6 +17,8 @@ module signal_decode(
     logic [31:0] imm_next;
     logic [2:0] ALUOp_next;
     logic [6:0] opcode_next;
+    logic fu_mem_next;
+    logic fu_alu_next;
     
     always_comb begin
         opcode_next = instr[6:0];
@@ -26,6 +30,8 @@ module signal_decode(
                 rs2_next = 5'b0;
                 rd_next = instr[11:7];
                 ALUOp_next = 3'b011;
+                fu_alu_next = 1'b1;
+                fu_mem_next = 1'b0;
             end
             // LUI
             7'b0110111: begin
@@ -33,6 +39,8 @@ module signal_decode(
                 rs2_next = 5'b0;
                 rd_next = instr[11:7];
                 ALUOp_next = 3'b100;
+                fu_alu_next = 1'b1;
+                fu_mem_next = 1'b0;
             end
             // R-type instructions
             7'b0110011: begin
@@ -40,6 +48,8 @@ module signal_decode(
                 rs2_next = instr[24:20];
                 rd_next = instr[11:7];
                 ALUOp_next = 3'b010;
+                fu_alu_next = 1'b1;
+                fu_mem_next = 1'b0;
             end
             // Load instructions excluding LUI 
             7'b0000011: begin
@@ -47,6 +57,8 @@ module signal_decode(
                 rs2_next = 5'b0;
                 rd_next = instr[11:7];
                 ALUOp_next = 3'b000;
+                fu_alu_next = 1'b1;
+                fu_mem_next = 1'b1;
             end
             // S-type instructions
             7'b0100011: begin
@@ -54,6 +66,8 @@ module signal_decode(
                 rs2_next = instr[24:20];
                 rd_next = 5'b0;
                 ALUOp_next = 3'b000;
+                fu_alu_next = 1'b1;
+                fu_mem_next = 1'b1;
             end
             // BNE
             7'b1100011: begin
@@ -61,6 +75,8 @@ module signal_decode(
                 rs2_next = instr[24:20];
                 rd_next = 5'b0;
                 ALUOp_next = 3'b001;
+                fu_alu_next = 1'b1;
+                fu_mem_next = 1'b0;
             end
             // JALR
             7'b1100111: begin
@@ -68,6 +84,8 @@ module signal_decode(
                 rs2_next = 5'b0;
                 rd_next = instr[11:7];
                 ALUOp_next = 3'b110;
+                fu_alu_next = 1'b1;
+                fu_mem_next = 1'b0;
             end
             // For other unknown OPcodes
             default: begin
@@ -75,6 +93,8 @@ module signal_decode(
                 rs2_next = 5'b0;
                 rd_next = 5'b0;
                 ALUOp_next = 3'b0;
+                fu_alu_next = 1'b0;
+                fu_mem_next = 1'b0;
             end
         endcase
     end
